68c5da0843fa84eeec488a5eb4e9a2329a71010b
Christo Stephenson <christo@blueskylabs.com.au>
2020-05-18 19:49:27 +0800

    dump unmatched items to file

--- findtemplates.py
+++ findtemplates.py
@@ -1,7 +1,7 @@
 import csv
 import re
 
-def printMatches(inputFileName, templatesFile):
+def printNoMatches(inputFileName, templatesFile):
 
     # read contents
     try:
@@ -32,3 +32,56 @@ def printMatches(inputFileName, templatesFile):
             if regex.match(input["CondText"]):
                 matches += 1
                 break
+
+def dumpUnmatchedRows(inputFileName, templatesFile, outputFileName):
+
+    # read input headers
+    try:
+        with open(inputFileName, "r") as csvInput:
+            reader = csv.reader(csvInput)
+            headers = next(reader)
+    except IOError:
+        print("File not found: " + inputFileName)
+        return 1
+
+    # read input contents
+    try:
+        with open(inputFileName, "r") as csvInput:
+            inputList = list(csv.DictReader(csvInput))
+    except IOError:
+        print("File not found: " + inputFileName)
+        return 1
+
+    # read templates
+    try:
+        with open(templatesFile, "r") as csvInput:
+            templatesList = list(csv.DictReader(csvInput))
+    except IOError:
+        print("File not found: " + inputFileName)
+        return 1
+
+    regexExpressions = []
+    for template in templatesList:
+        regexExpressions.append(re.compile(template["Regex"]))
+
+    # write
+    with open(outputFileName, "w") as csvOutput:
+        writer = csv.DictWriter(csvOutput, fieldnames=headers, quoting=csv.QUOTE_NONNUMERIC)
+
+        writer.writeheader()
+
+        count = 0
+        for row in inputList:
+            count += 1
+            if count % 10000 == 0:
+                print(count)
+            matchFound = False
+            for regex in regexExpressions:
+                if regex.match(row["CondText"]):
+                    matchFound = True
+                    break
+
+            if not matchFound:
+                writer.writerow(row)
+
+        print("Output: " + outputFileName)
--- main.py
+++ main.py
@@ -6,7 +6,8 @@ def main():
     # sampleset.generateSampleSet()
     # miningTenementTemplate.fix("tenement_templates_dupes_removed.csv", "tenement_templates_fixed.csv")
     # miningTenementTemplate.generateRegex("tenement_templates_fixed.csv", "tenement_templates_regex.csv")
-    findtemplates.printMatches("Conditions.csv", "tenement_templates_regex.csv")
+    # findtemplates.printNoMatches("Conditions.csv", "tenement_templates_regex.csv")
+    findtemplates.dumpUnmatchedRows("Conditions.csv", "tenement_templates_regex.csv", "ConditionsNoRegexMatches.csv")
 
 if __name__ == '__main__':
     main()
